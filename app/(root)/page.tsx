91ec059d8e39acbe03f25f2e5b94c3e27db74814
Favour Ebuka <favourvanderwaal@gmail.com>
2024-09-04 12:53:13 +0100

    implement home sideBar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Shaw" };
+  const loggedIn = {
+    firstName: "Shaw",
+    lastName: "favour",
+    email: "favourvanderwaal@gmail.com",
+  };
 
   return (
     <section className="home">
@@ -21,7 +26,14 @@ const Home = () => {
             totalCurrentBalance={1251.66}
           />
         </header>
+        Recent transaction
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 1128.5 }]}
+      />
     </section>
   );
 };
